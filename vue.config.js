2e9092445f930b6115abcf74a00ca2b3cddc99ee
FlatoupiX <pnowak@levallois.eudoweb.com>
2019-05-23 16:49:58 +0200

    Ajout de vuetify et quelques infos dans vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,21 @@
 module.exports = {
   filenameHashing: false,
-  publicPath: "",
-  outputDir: "build",
+  publicPath: '',
+  outputDir: 'build',
   //productionSourceMap: false,
   configureWebpack: {
-    devtool: "source-map"
+    devtool: 'source-map',
   },
   devServer: {
     proxy: {
-      
-      '/services': { // Repertoire virtuel à mettre en suffix à toutes les requêtes AJAX
+      '/services': {
+        // Repertoire virtuel à mettre en suffix à toutes les requêtes AJAX
         target:
-          'http://ext.adress.com/' // Adresse cible sur laquelle se trouve les web services. 
-          
-          + 'Specif/EUDO_PRODUIT_PUBLIC/root/ProduitPublic/gru/demande', // Exemple de chemin extact d'accès aux websevices/
+          'http://ext.adress.com/' + 'Specif/EUDO_PRODUIT_PUBLIC/root/ProduitPublic/gru/demande', // Adresse cible sur laquelle se trouve les web services. // Exemple de chemin extact d'accès aux websevices/
       },
     },
   },
-};
+}
+// Attention, toutes les modifications que vous serez amenés à faire au sein de se fichier ne seront pas prises en compte avec
+// le "hot reload" du npm run serve.
+// Il faudra donc relancer cette commande.
