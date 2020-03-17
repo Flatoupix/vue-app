559b91fe37204625a2368a2ff7d4304bbe88f4f5
flatoupix <=>
2020-03-17 18:00:57 +0100

    mise à jour du template à la version 0.1.19 ainsi que ses dépendances

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,135 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+  <v-app :ripple="false">
+    <!-- <logo class="mx-auto" :style="{ width: '30em' }"></logo> -->
+    <v-form ref="form">
+      <v-container @click.ctrl="updateTime()">
+        <v-row class="align-center justify-space-around">
+          <edn-load message="edn-load" form="double" anim="dots"></edn-load>
+          <edn-load message="edn-load" form="simple" anim="blink"></edn-load>
+          <edn-load message="edn-load" form="fill"></edn-load>
+        </v-row>
+        <edn-num label="edn-num" v-model="inptNum" tooltip="NOMBRE"></edn-num>
+        <edn-field label="edn-field" v-model="inptText"></edn-field>
+        <edn-cat label="edn-cat" v-model="selectedCat" :items="cat"></edn-cat>
+        <edn-cat-x
+          label="edn-cat-x"
+          v-model="ingredient"
+          :items="recette"
+          tooltip="Selection de catégories"
+        ></edn-cat-x>
+        <edn-date v-model="dateInpt" label="edn-date" :popup="true" required />
+        <edn-time
+          v-model="time"
+          label="edn-time"
+          required="Ce champs doit être rempli !"
+        />
+        <v-row class="align-center justify-center">
+  
+            <edn-date
+              v-model="dateInpt"
+              :popup="false"
+              :allowed-dates="allowedDates"
+              class="mx-3"
+            />
+            <edn-time
+              class="mx-3"
+              v-model="time"
+              label="Choisissez une plage horaire"
+              tripStyle
+              :slots="[
+                '00:20',
+                '00:40',
+                '01:20',
+                '01:40',
+                '02:40',
+                '03:40',
+                '04:40',
+                '05:40',
+                '06:40'
+              ]"
+            />
+         
+        </v-row>
+        <edn-memo
+          label="edn-memo"
+          v-model="inptMemo"
+          :size="'normal'"
+          html
+          :required="'Un string'"
+        ></edn-memo>
+        <edn-mail label="edn-mail" v-model="mail" :required="true" />
+        <edn-phone label="edn-phone" v-model="phone"></edn-phone>
+        <edn-check
+          label="edn-check"
+          v-model="checked"
+          :required="true"
+        ></edn-check>
+        <edn-switch label="edn-switch" v-model="switched"></edn-switch>
+        <edn-radio
+          label="edn-radio"
+          :radios="radios"
+          v-model="radioSelect"
+        ></edn-radio>
+        <edn-color label="edn-color" v-model="selectedColor"></edn-color>
+        <v-row class="justify-center align-self-center">
+          <edn-btn @click.native="Validate()">Valider</edn-btn>
+          <edn-btn @click.native="Reset()" alternate>Reset</edn-btn>
+        </v-row>
+      </v-container>
+    </v-form>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: "App",
+
+  data() {
     return {
-      //
+      civility: ["M.", "Mme"],
+      msgRequis: "Champs requis !",
+      citiesList: [],
+      loading: false,
+      inptNum: null,
+      mail: "",
+      checked: true,
+      switched: true,
+      allowedDates: val => parseInt(val.split("-")[2], 10) % 2 === 0,
+      inptText: "",
+      dateInpt: "",
+      inptMemo: "",
+      ingredient: [],
+      recette: ["Cat 3", "Cat 4", "Cat 5"],
+      cat: ["Cat 1", "Cat 2", "Cat 3", "Cat 4", "Cat 5"],
+      selectedCat: "",
+      radioSelect: null,
+      selectedColor: "",
+      radios: [
+        {
+          label: "Choix 1",
+          value: "1"
+        },
+        {
+          label: "Choix 2",
+          value: "2"
+        }
+      ],
+      contentAbc: "abcdefghijklmnopqtrsuvwxyz",
+      date: "",
+      time: "",
+      phone: ""
+    };
+  },
+  methods: {
+    updateTime() {
+      this.time = "09:00";
+    },
+    Validate() {
+      this.$refs.form.validate();
+    },
+    Reset() {
+      this.$refs.form.reset();
     }
   }
-}
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,47 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
+import Vue from "vue";
 import router from './router'
 import store from './store'
+import App from "./App.vue";
+import Vuetify from "vuetify";
+import fr from "vuetify/es5/locale/fr";
+import eudoFront from "eudo-front";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(eudoFront);
+
+const opts = {
+  icons: {
+    iconfont: "mdi"
+  },
+  theme: {
+    options: {
+      customProperties: true
+    },
+    themes: {
+      light: {
+        primary: "#bb1515",
+        secondary: "#757575",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107"
+      }
+    }
+  },
+  lang: {
+    locales: { fr },
+    current: "fr"
+  }
+};
+Vue.use(Vuetify);
+
+new Vue({
+  vuetify: new Vuetify(opts),
+  router, store,
+  render: h => h(App)
+}).$mount("#app");
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
